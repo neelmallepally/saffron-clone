3d84dac9f1f3fc424d876ec4dcff4a335a98ee05
Neel Mallepally <nmallepally@smartbutton.com>
2019-09-30 21:31:15 -0400

    change IHostEnvironment to IWebHostEnvironment

--- Saffron.API/Startup.cs
+++ Saffron.API/Startup.cs
@@ -39,7 +39,7 @@ namespace Saffron.API
 
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			app.UseDefaultFiles();
 			app.UseStaticFiles();

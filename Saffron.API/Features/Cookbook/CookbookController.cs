c0fc96354ac618bf992b602554e8df95926de496
Neel Mallepally <neel.mallepally@aimia.com>
2019-11-24 04:43:22 -0500

    WIP

---
+++ Saffron.API/Features/Cookbook/CookbookController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Saffron.API.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Saffron.API.Features.Cookbook
+{
+	[Route("api/cookbooks")]
+	[ApiController]
+	public class CookbookController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public CookbookController(IMediator mediator) => _mediator = mediator;
+
+		//public async Task<IActionResult> GetAll()
+		//{
+		//	var cookbookDto = await _mediator.Send<CookbookDTO>(new CookbookAllQuery());
+		//}
+	}
+
+	public class CookbookDTO
+	{
+
+	}
+	public class CookbookAllQuery : IRequest<CookbookDTO>
+	{
+	}
+
+	public class CookbookAllQueryHandler : IRequestHandler<CookbookAllQuery, CookbookDTO>
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CookbookAllQueryHandler(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<CookbookDTO> Handle(CookbookAllQuery request, CancellationToken cancellationToken)
+		{
+			var result = new CookbookDTO();
+
+			//TO DO: Modify this cod
+			var cookbooks = await _context.Cookbooks.AsNoTracking().ToListAsync();
+			return result;
+		}
+	}
+
+}
--- Saffron.API/Startup.cs
+++ Saffron.API/Startup.cs
@@ -1,40 +1,29 @@
-﻿using GraphQL;
-using GraphQL.Http;
-using GraphQL.Types;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Saffron.API.Data;
-using Saffron.API.Data.Abstractions;
-using Saffron.API.Data.Repositories;
-using Saffron.API.Middleware;
 
 namespace Saffron.API
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		public void ConfigureServices(IServiceCollection services)
 		{
-
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-			//TODO: move connection string value to configuration file
-			var connection = @"Server=(localdb)\mssqllocaldb;Database=SaffronClone;Trusted_Connection=True;ConnectRetryCount=0";
-			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-			services.AddScoped<ICookbookRepository, CookbookRepository>();
-			services.AddScoped<IRecipeRepository, RecipeRepository>();
-
-			services.AddScoped<IDocumentWriter, DocumentWriter>();
-			services.AddScoped<IDocumentExecuter, DocumentExecuter>();
-
-			services.AddScoped<SaffronQuery>();
-			services.AddScoped<SaffronMutation>();
-			services.AddScoped<ISchema, SaffronSchema>();
 		}
 
 
@@ -44,7 +33,6 @@ namespace Saffron.API
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
 
-			app.UseMiddleware<GraphQLMiddleware>();
 		}
 	}
 }
